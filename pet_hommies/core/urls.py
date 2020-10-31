30b09a4cd756399286b304ce3538386ed6bd7134
strabbo <crazyforthelakers@gmail.com>
2020-10-31 19:05:57 +0300

    super design

--- pet_hommies/core/urls.py
+++ pet_hommies/core/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 from rest_framework_swagger.views import get_swagger_view
-
-from .views import AnimalView, MainAnimalInfoView, AnimalCardView
-
+from .views import AnimalView, AnimalTableInfo, MainAnimalInfoView, AnimalCardView
 app_name = "animals"
 # app_name will help us do a reverse look-up latter.
 schema_view = get_swagger_view(title='Pets\' Hommies Api')
 
 urlpatterns = [
     path('animals/', AnimalView.as_view()),
+    path('table/', AnimalTableInfo),
     path('get_animals/', MainAnimalInfoView.as_view()),
     path('swagger/', schema_view),
     path('card/<animal_id>/', AnimalCardView.as_view()),
