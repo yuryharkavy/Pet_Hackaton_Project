7265d0a1269f3bc0f2e32c37e8a9c30caacf03d5
Dima <dimasce@ozon.ru>
2020-10-31 10:19:18 +0300

    new models descrip

--- pet_hommies/core/models.py
+++ pet_hommies/core/models.py
@@ -9,7 +9,27 @@ class AnimalKind(models.Model):
 
 class Breed(models.Model):
     animal_kind = models.ForeignKey(AnimalKind, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+
+
+class Gender(models.Model):
     name = models.CharField(max_length=30, primary_key=True)
+
+
+class Tail(models.Model):
+    tail_type = models.TextField
+
+
+class DepartureReason(models.Model):
+    reason_type = models.TextField
+
+
+class DeathReason(models.Model):
+    death_reason = models.TextField
+
+
+class EuthanasiaReason(models.Model):
+    reason = models.TextField
 
 
 class Animal(models.Model):
@@ -19,7 +39,9 @@ class Animal(models.Model):
     nickname = models.CharField(max_length=20)
     tips = models.TextField
     cell = models.IntegerField
+    tail = models.ForeignKey(Tail, on_delete=models.CASCADE)
     breed = models.ForeignKey(Breed, on_delete=models.CASCADE)
+    gender = models.ForeignKey(Gender, on_delete=models.CASCADE)
     mark_id = models.IntegerField(primary_key=True)
     date_sterilization = models.CharField(max_length=30)
     socialized = models.BooleanField
@@ -36,6 +58,7 @@ class Animal(models.Model):
     knock_out_reason = models.TextField
     knock_out_id = models.CharField(primary_key=True, max_length=20)
     worker_name = models.CharField(max_length=50)
+    departure_reason = models.ForeignKey(DepartureReason, on_delete=models.CASCADE)
     parasite_treatment_id = models.IntegerField
     parasite_treatment_date = models.DateField
     parasite_treatment_drug = models.CharField(max_length=50)
