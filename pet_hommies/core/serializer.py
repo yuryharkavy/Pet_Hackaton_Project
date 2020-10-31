6ab48563040df729c638dad8e39dba1385cdf57e
Dima <dimasce@ozon.ru>
2020-10-31 15:03:39 +0300

    asd

--- pet_hommies/core/serializer.py
+++ pet_hommies/core/serializer.py
@@ -1,5 +1,45 @@
 from rest_framework import serializers
 
+from .models import Animal
 
-class AnimalSerializer(serializers.Serializer):
+
+class AnimalTypeSerializer(serializers.Serializer):
     name = serializers.CharField()
+
+
+class AnimalInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Animal
+        fields = ['card_id', 'age',
+                  'weight',
+                  'nickname',
+                  'tips',
+                  'cell',
+                  'tail',
+                  'breed', 'gender', 'mark_id',
+                  'date_sterilization',
+                  'socialized',
+                  'admission_id',
+                  'admission_id_date',
+                  'catch_id',
+                  'catch_address',
+                  'legal_entity',
+                  'holder_name',
+                  'individual_entity',
+                  'admission_date',
+                  'admission_id_2',
+                  'knock_out_date',
+                  'knock_out_reason',
+                  'knock_out_id',
+                  'worker_name',
+                  'departure_reason',
+                  'parasite_treatment_id',
+                  'parasite_treatment_date',
+                  'parasite_treatment_drug',
+                  'parasite_treatment_drug_dose',
+                  'vaccine_id',
+                  'vaccine_dates',
+                  'vaccine_type',
+                  'vaccine_series',
+                  'inspection_date',
+                  'inspection_result']
--- pet_hommies/core/views.py
+++ pet_hommies/core/views.py
@@ -2,16 +2,22 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import AnimalKind
-from .serializer import AnimalSerializer
+from .models import AnimalKind, Animal
+from .serializer import AnimalTypeSerializer
 
 
 class AnimalView(APIView):
     def get(self, request):
-        articles = AnimalKind.objects.all()
-        serializer = AnimalSerializer(articles, many=True)
+        animal_type = AnimalKind.objects.all()
+        serializer = AnimalTypeSerializer(animal_type, many=True)
+
         return Response({"animal types": serializer.data})
 
     def post(self, request):
         return Response({"animal types": request.data['hello']})
 
+
+# class AnimalInfoView(APIView):
+#     def get(self):
+#         animals = Animal.objects.filter(socialized=True)
+#         serializer = AnimalTypeSerializer(animals, many=True)
